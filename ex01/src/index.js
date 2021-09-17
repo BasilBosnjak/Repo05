73f4aff1957b74ed0425a398ef69549087b04fd7
BasilBosnjak <basil.bosnjak@gmail.com>
2021-09-17 19:17:52 +0200

    Adding exercise 01

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+
+    var myFirstString, mySecondString;
+    myFirstString = "I think therefore I \"am\" -- sentient.";
+    mySecondString = "No, you only \"think\", so you aren't."
+
+    return {
+        myFirstString,
+        mySecondString
+    };
+}
+
+console.log(main());
+module.exports = main;
